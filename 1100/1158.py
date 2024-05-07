3ebdd5c6e49fb247537165bf799b216586543ecd
SungKyung-Shon <220290658@aston.ac.uk>
2024-05-07 15:01:02 +0100

    add file

---
+++ 1100/1158.py
@@ -0,0 +1,19 @@
+import sys
+from collections import deque
+
+people = deque()
+
+n, k = map(int, sys.stdin.readline().split())
+
+for i in range(1, n+1):
+    people.append(i)
+
+result = []
+i = 0
+while len(people) > 0:
+    for j in range(1, k):
+        #people.append(people.popleft())
+        people.popleft()
+    result.append(people.popleft())
+
+print(str(result).replace('[', '<').replace(']', '>'))
