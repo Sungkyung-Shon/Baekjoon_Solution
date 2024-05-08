5606852d13bc998a00dda97042fb9b2a6b69cd06
SungKyung-Shon <220290658@aston.ac.uk>
2024-05-08 14:49:14 +0100

    add file

--- 1100/1158.py
+++ 1100/1158.py
@@ -1,19 +1,36 @@
+# import sys
+# from collections import deque
+
+# people = deque()
+
+# n, k = map(int, sys.stdin.readline().split())
+
+# for i in range(1, n+1):
+#     people.append(i)
+
+# result = []
+# i = 0
+# while len(people) > 0:
+#     for j in range(1, k):
+#         people.append(people.popleft())
+#     result.append(people.popleft)
+
+# print(str(result).replace('[', '<').replace(']', '>'))
+
+
 import sys
 from collections import deque
 
-people = deque()
-
 n, k = map(int, sys.stdin.readline().split())
+people = deque
 
 for i in range(1, n+1):
-    people.append(i)
+    people.append()
 
 result = []
-i = 0
-while len(people) > 0:
-    for j in range(1, k):
-        #people.append(people.popleft())
-        people.popleft()
-    result.append(people.popleft())
+for i in range(1, n+1):
+    for j in range(1,k):
+        people.append(people.popleft())
+    result.append(people.popleft)
 
 print(str(result).replace('[', '<').replace(']', '>'))
