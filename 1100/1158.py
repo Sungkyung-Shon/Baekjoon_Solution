38d5e68b84659028561b5df878cbd0dac6416be8
SungKyung-Shon <220290658@aston.ac.uk>
2024-05-09 02:43:13 +0100

    add file

--- 1100/1158.py
+++ 1100/1158.py
@@ -1,35 +1,17 @@
-# import sys
-# from collections import deque
-
-# people = deque()
-
-# n, k = map(int, sys.stdin.readline().split())
-
-# for i in range(1, n+1):
-#     people.append(i)
-
-# result = []
-# i = 0
-# while len(people) > 0:
-#     for j in range(1, k):
-#         people.append(people.popleft())
-#     result.append(people.popleft)
-
-# print(str(result).replace('[', '<').replace(']', '>'))
-
-
 import sys
 from collections import deque
 
+people = deque()
+
 n, k = map(int, sys.stdin.readline().split())
-people = deque
 
 for i in range(1, n+1):
-    people.append()
+    people.append(i)
 
 result = []
-for i in range(1, n+1):
-    for j in range(1,k):
+i = 0
+while len(people) > 0:
+    for j in range(1, k):
         people.append(people.popleft())
     result.append(people.popleft)
 
---
+++ 2800/2805.py
@@ -0,0 +1,26 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+tree = list(map(int, sys.stdin.readline().split()))
+#tree.sort()
+
+left, right = 0, max(tree)
+result = 0 
+while left < right:
+    num = 0
+    mid = (left + right) // 2  
+    for i in tree:
+        if i > mid:
+            num += i - mid
+    if num < M:
+        right = mid - 1
+    else:
+        result = mid
+        left = mid + 1
+print(result)
+    
+
+        
+
+
+
