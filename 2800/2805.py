dbd992e88b9a2ed290098f30b270c3a4c10cc3b3
SungKyung-Shon <220290658@aston.ac.uk>
2024-05-09 22:08:02 +0100

    add file

--- 2800/2805.py
+++ 2800/2805.py
@@ -2,11 +2,10 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 tree = list(map(int, sys.stdin.readline().split()))
-#tree.sort()
 
 left, right = 0, max(tree)
 result = 0 
-while left < right:
+while left <= right:
     num = 0
     mid = (left + right) // 2  
     for i in tree:
@@ -17,10 +16,4 @@ while left < right:
     else:
         result = mid
         left = mid + 1
-print(result)
-    
-
-        
-
-
-
+print(result)  
