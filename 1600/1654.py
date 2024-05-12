8e8aa9370a642c61245eca6521634339eead67b4
SungKyung-Shon <220290658@aston.ac.uk>
2024-05-12 22:08:41 +0100

    add file

--- 1600/1654.py
+++ 1600/1654.py
@@ -1,23 +1,22 @@
 import sys
-from collections import deque
 
 n, k = map(int, sys.stdin.readline().split())
-cable = deque()
 
-for i in range(1, n+1):
+cable = []
+
+for i in range(n):
     cable.append(int(input()))
 
-left, right = 1, max(cable)  
+left, right = 1, max(cable)
 while left <= right:
-    x = 0 
-    mid = (left + right) // 2
+    x =0
+    mid = (left+right) // 2
     for j in cable:
         if j >= mid:
-            x += j//mid  
+            x += j//mid
     if x < k:
         right = mid-1
     else:
         result = mid
         left = mid+1
 print(result)
-
