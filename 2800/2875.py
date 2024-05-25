475f29691c31091bf5a96fb3c72b4a31d9dfd3d8
Sung.K Shon <81243837+Sungkyung-Shon@users.noreply.github.com>
2024-05-25 15:32:28 +0100

    Update 2875.py

--- 2800/2875.py
+++ 2800/2875.py
@@ -1,1 +1,10 @@
-n, m, k = map(int, input().split())
+n, m, k = map(int, input().split())   # n: 여학생 수, m: 남학생 수, k: 인턴쉽 학생 수
+team = 0
+
+while n>=2 and m>=1 and n+m >= k+3:  # 2명,1명 팀 만들수 있고, 인턴쉽도 보낼 수 있는 수일때
+    n -= 2
+    m -= 1
+    team += 1
+print(team)
+
+
