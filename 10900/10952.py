06922c851ae9372d5cb38e7259768f200941838e
SungKyung-Shon <220290658@aston.ac.uk>
2024-07-08 18:21:23 +0100

    add file

---
+++ 10900/10952.py
@@ -0,0 +1,7 @@
+#python
+# while True:
+#     a, b = map(int, input().split())
+#     if a == 0 and b ==0:
+#         break
+#     else:
+#         print(a+b)
---
+++ 27800/27866.py
@@ -0,0 +1,2 @@
+s = str(input())
+i = int(input())
