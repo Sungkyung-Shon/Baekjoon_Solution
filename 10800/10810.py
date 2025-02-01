1a9a52df7bb7a489803543976dace9f40e24385f
SungKyung-Shon <sungkshon@gmail.com>
2025-02-01 14:04:26 +0900

    add file

---
+++ 10800/10810.py
@@ -0,0 +1,21 @@
+# n,m = map(int,input().split())
+# box = [0]*n  #바구니 수만큼
+
+# for _ in range(m):
+#     i,j,k = map(int,input().split())
+#     for idx in range(i,j+1):
+#         box[idx-1] = k
+
+# for i in range(n):
+#     print(box[i], end=" ")
+
+
+n,m = map(int,input().split())
+box = [0] *n
+
+for _ in range(m):
+    i,j,k = map(int,input().split())
+    for idx in range(i,j+1):
+        box[idx-1]=k
+for i in range(n):
+    print(box[i], end=" ")
--- 3000/3052.py
+++ 3000/3052.py
@@ -1,6 +1,13 @@
-arr = []
+# arr = []
+# for i in range(10):
+#     a = int(input())
+#     if a % 42 not in arr:
+#         arr.append(a%42)
+# print(len(arr))
+
+result = []
 for i in range(10):
     a = int(input())
-    if a % 42 not in arr:
-        arr.append(a%42)
-print(len(arr))
+    if a%42 not in result:
+        result.append(a%42)
+print(len(result))
