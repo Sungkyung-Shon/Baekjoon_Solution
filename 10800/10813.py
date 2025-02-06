0cef7ad979a1f3cb9b297e1850b29930061d636c
SungKyung-Shon <sungkshon@gmail.com>
2025-02-06 22:03:25 +0900

    add file

--- 10800/10813.py
+++ 10800/10813.py
@@ -1,11 +1,24 @@
-n,m = map(int, input().split())
-box = [i for i in range(1, n+1)]
+# n,m = map(int, input().split())
+# box = [i for i in range(1, n+1)]
+
+# for _ in range(m):
+#     i, j = map(int, input().split())
+#     temp = box[i-1]
+#     box[i-1] = box[j-1]
+#     box[j-1] = temp
+
+# for i in range(n):
+#     print(box[i], end=" ")
+
+
+n,m = map(int,input().split())
+box = [i for i in range(1,n+1)]
 
 for _ in range(m):
-    i, j = map(int, input().split())
+    i,j = map(int,input().split())
     temp = box[i-1]
     box[i-1] = box[j-1]
-    box[j-1] = temp
+    box[j-1]= temp
 
 for i in range(n):
     print(box[i], end=" ")
--- 3000/3052.py
+++ 3000/3052.py
@@ -21,6 +21,14 @@
 # print(len(result))
 
 
+# result = []
+# for i in range(10):
+#     a = int(input())
+#     if a%42 not in result:
+#         result.append(a%42)
+# print(len(result))
+
+
 result = []
 for i in range(10):
     a = int(input())
